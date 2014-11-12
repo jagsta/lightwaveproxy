716383499d25702ef85859ad509d22b22a6af10d
Jag Bains <git@jagbains.org>
2014-11-12 15:31:35 +0000

    listen to any topic, changed to modify switch for testing

--- domoticz_subscriber.js
+++ domoticz_subscriber.js
@@ -2,7 +2,7 @@ var http = require('http');
 var mqtt = require('/usr/local/lib/node_modules/mqtt')
   , client = mqtt.connect('mqtts://orkeplpr:H8xTakU5Hhbv@m20.cloudmqtt.com:20280?clientId=nook');
 
-client.subscribe('messages');
+client.subscribe('#');
 client.on('message', function(topic, message) {
   try{
       object=JSON.parse(message);
@@ -10,19 +10,20 @@ client.on('message', function(topic, message) {
       console.log('Parse to JSPN failed: ' + message,e); //error in the above string(in this case,yes)!
   }
   var request = 'http://192.168.100.112:8000/json.htm?type=command'
+  console.log('topic: ' + topic);
   if (object.command && object.device) {
       console.log(object.device, object.command);
-      if (object.device.match(/exus5/)) {
-          console.log('matched device');
-  	request += '&param=updateuservariable&idx=1&vname=Nexus5&vtype=2';
+      if (object.device.match(/jag/)) {
+          console.log('matched device jag');
+  	request += '&param=switchlight&idx=29';
       }
       if (object.command.match(/on/)){
           console.log('matched on');
-  	request += '&vvalue=1';
+  	request += '&switchcmd=1&level=0';
       }
       else if (object.command.match(/off/)){
           console.log('matched off');
-  	request += '&vvalue=0';
+  	request += '&switchcmd=0&level=0';
       }
     console.log(request);
     var req = http.get(request, function(res) {
