877ff29f55a8cb5a715ffb07dbed42e4dc840f49
Jag Bains <git@jagbains.org>
2014-11-12 14:35:46 +0000

    extensive changes to domoticz subscriber, jsonified the input message parsing and added error handling for parsing

--- domoticz_subscriber.js
+++ domoticz_subscriber.js
@@ -1,7 +1,37 @@
+var http = require('http');
 var mqtt = require('/usr/local/lib/node_modules/mqtt')
   , client = mqtt.connect('mqtts://orkeplpr:H8xTakU5Hhbv@m20.cloudmqtt.com:20280?clientId=nook');
 
 client.subscribe('messages');
 client.on('message', function(topic, message) {
-  console.log(message);
+  try{
+      object=JSON.parse(message);
+  }catch(e){
+      console.log('Parse to JSPN failed: ' + message,e); //error in the above string(in this case,yes)!
+  }
+  var request = 'http://192.168.100.112:8000/json.htm?type=command'
+  if (object.command && object.device) {
+      console.log(object.device, object.command);
+      if (object.device.match(/exus5/)) {
+          console.log('matched device');
+  	request += '&param=updateuservariable&idx=1&vname=Nexus5&vtype=2';
+      }
+      if (object.command.match(/on/)){
+          console.log('matched on');
+  	request += '&vvalue=1';
+      }
+      else if (object.command.match(/off/)){
+          console.log('matched off');
+  	request += '&vvalue=0';
+      }
+    console.log(request);
+    var req = http.get(request, function(res) {
+     console.log("Got response: " + res.statusCode);
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
+  
+  };
+    //console.log(message);
+  
 });
